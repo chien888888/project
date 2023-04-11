0834fc89ba786034181986a7d048780948296d61
chien888888 <chien5397@gmail.com>
2023-04-11 19:33:10 +0800

    0411-1932

---
+++ js/about.js
@@ -0,0 +1,19 @@
+$(function () {
+
+
+    $(function () {
+
+
+        $('.accordion-header').click(function () {
+            $(this).parent().toggleClass('active');
+
+
+            $(this).next().slideToggle();
+        })
+
+
+    })
+
+})
+
+
